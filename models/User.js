ac2b27accabb53de106b8f4c53c270bd0c7d1581
Haris Mughal <harismirza3456@gmail.com>
2024-08-03 01:18:05 +0500

    user model created

---
+++ models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  sportPref: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
